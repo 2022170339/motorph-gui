736f28dc6eb94e199e6e0d1f8e7e3fc879605391
redenv <redenv@careindeed.com>
2023-05-16 18:44:57 +0800

    Initial commit

---
+++ src/main/java/com/mmdc/motorph/MotorPH.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mmdc.motorph;
+
+/**
+ *
+ * @author redenval
+ */
+public class MotorPH {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
